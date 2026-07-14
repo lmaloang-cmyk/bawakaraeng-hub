403e913dbcdbc16b99889692b3ec7d07da492926
lmaloang-cmyk <lmaloang@gmail.com>
2026-07-14 14:52:08 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='bwk-v8';
+const CACHE='bwk-v12';
 const ASSETS=['/','/index.html','/manifest.json','/icon-192.png','/icon-512.png','/apple-touch-icon.png','/rc-logo.jpg','/species-macaca.jpg','/og.jpg'];
 self.addEventListener('install',function(e){
   self.skipWaiting();
